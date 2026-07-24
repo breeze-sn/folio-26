a6cfc338e9ca2f537bbf0dbcdc2901b1324cecc4
Simran Nagekar <134700073+breeze-sn@users.noreply.github.com>
2026-07-24 18:09:36 +0000

    spotifyfixagain

--- api/spotify/connect.js
+++ api/spotify/connect.js
@@ -2,7 +2,12 @@ import { createState, isConfigured, spotifyConfig } from "./_shared.js";
 
 export default function handler(request, response) {
   const config = spotifyConfig();
-  if (!isConfigured(config) || request.query.token !== config.connectToken) {
+  if (!isConfigured(config)) {
+    return response.status(503).send(
+      "Spotify is not configured. Add SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET, SPOTIFY_REDIRECT_URI, and SPOTIFY_CONNECT_TOKEN in Vercel, then redeploy."
+    );
+  }
+  if (request.query.token !== config.connectToken) {
     return response.status(404).end();
   }
 
